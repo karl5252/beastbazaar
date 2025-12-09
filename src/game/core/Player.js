27e8079f2b870c4a98304f040d6fbb63940f38f8
karl5252 <karl.michalik@gmail.com>
2025-12-09 22:19:19 +0100

    Implement Player class with herd management and add unit tests

---
+++ src/game/core/Player.js
@@ -0,0 +1,50 @@
+
+export class Player {
+    constructor(name="testPlayer", index=0) {
+        this.name = name;
+        this.index = index;
+        this.herd = {
+            "Rabbit": 0,
+            "Sheep": 0,
+            "Pig": 0,
+            "Cow": 0,
+            "Horse": 0,
+            "Foxhound": 0,
+            "Wolfhound": 0
+        };
+
+    }
+
+    getHerd() {
+        return this.herd;
+    }
+
+    updateHerd(animalType, count) {
+        this.herd[animalType] = count;
+    }
+
+    transfer_animal(recipientPlayer, animalType, count) {
+        // check if animal type is in herd at all if not return false
+
+        if(!(animalType in this.herd)) {
+            return false;
+        }
+        // get available count
+        let availableCount = self.getHerd(animalType, 0);
+        if (availableCount >= count) {
+            // deduct from sender
+            this.herd[animalType] -= count;
+            // add to recipient
+            let recipientCount = recipientPlayer.getHerd()[animalType] || 0;
+            recipientPlayer.updateHerd(animalType, recipientCount + count);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    transfer_from(senderPlayer, animalType, count) {
+        return senderPlayer.transfer_animal(this, animalType, count);
+    }
+
+}
---
+++ tests/core/player.test.js
@@ -0,0 +1,26 @@
+import {Player} from "../../src/game/core/Player.js";
+
+describe("player test", () => {
+    let player;
+
+    beforeEach(() => {
+        player = new Player("testPlayer", 0); // <-- przypisanie, nie deklaracja
+    });
+
+    it("get players herd", () => {
+        expect(player.getHerd()).toEqual({
+            "Rabbit": 0,
+            "Sheep": 0,
+            "Pig": 0,
+            "Cow": 0,
+            "Horse": 0,
+            "Foxhound": 0,
+            "Wolfhound": 0
+        });
+    })
+
+    it("update players herd", () => {
+        player.updateHerd("Sheep", 11);
+        expect(player.getHerd()["Sheep"]).toBe(11)
+    })
+})
