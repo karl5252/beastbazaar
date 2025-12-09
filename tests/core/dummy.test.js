915321485854427f837f0a443e841ad4b5b14c00
karl5252 <karl.michalik@gmail.com>
2025-12-09 21:56:08 +0100

    Add testing setup with Vitest and create a dummy test

---
+++ tests/core/dummy.test.js
@@ -0,0 +1,5 @@
+describe("dummy test", () => {
+    it("works test", () => {
+        expect(1 + 1).toBe(2)
+    })
+})
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ export default defineConfig({
 	plugins: [],
 	server: { host: '0.0.0.0', port: 8000 },
 	clearScreen: false,
+    test: {
+        environment: 'node',
+        globals: true,
+        include: ['tests/**/*.test.js'],
+    },
 })
